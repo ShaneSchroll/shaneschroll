82123f7a413455a65d4ad68f0a908c9dfccacde0
Shane Schroll <schroll24@gmail.com>
2023-10-17 20:58:08 -0500

    cleaned up scss and js

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,10 +1,6 @@
 (function($) {
     $(document).ready(function() {
 
-        $('#menu-toggle').on('click', function() {
-            $('.x-bar').toggleClass('x-bar-active');
-        });
-
         var path = location.pathname.split('/');
         var get_url = window.location.href;
 
@@ -14,7 +10,7 @@
             $('.grid-item--link[href^="/' + path[1] + '"]').addClass('active');
         }
 
-        // set active link based on URL for sub menu items
+        // set active link based on URL for mockup sub menu items
         if( get_url.indexOf('mockup') > -1 && path[1] !== 'mockups' ) {
             $('.sub-level-item[href^="http://portfolio-site.local/mockup/' + path[2] + '"]').addClass('sub-active'); // local dev
             // $('.sub-level-item[href^="https://shaneschroll.dev/mockup/' + path[2] + '"]').addClass('sub-active'); // production
