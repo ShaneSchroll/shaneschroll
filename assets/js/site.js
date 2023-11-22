05117893fe3c2cea0781cdb60ede89173f271761
Shane Schroll <schroll24@gmail.com>
2023-11-21 20:13:53 -0600

    working tabs / working content area / working style changes / js needs cleaned up

--- assets/js/site.js
+++ assets/js/site.js
@@ -18,7 +18,95 @@
 
         // UI Tabs for documentation
         if ( get_url.indexOf('help-docs') > -1 ) {
-            $('#project-tabs').tabs();
+
+            /* mega ugly */
+
+            // cache selectors
+            let $tab_1 = $('#tab-1');
+            let $tab_2 = $('#tab-2');
+            let $tab_3 = $('#tab-3');
+            let $tab_4 = $('#tab-4');
+            let $tab_5 = $('#tab-5');
+
+            // initial state
+            $('#tab-1 .icon-box').addClass('tab-active');
+            $('#doc-tab-1').show();
+
+            $('#doc-tab-2').hide();
+            $('#doc-tab-3').hide();
+            $('#doc-tab-4').hide();
+            $('#doc-tab-5').hide();
+
+            // tab and content click events
+            $tab_1.on('click', function() {
+                $('#tab-1 .icon-box').addClass('tab-active');
+                $('#tab-2 .icon-box').removeClass('tab-active');
+                $('#tab-3 .icon-box').removeClass('tab-active');
+                $('#tab-4 .icon-box').removeClass('tab-active');
+                $('#tab-5 .icon-box').removeClass('tab-active');
+
+                $('#doc-tab-1').slideToggle(450);
+                $('#doc-tab-2').slideUp(450);
+                $('#doc-tab-3').slideUp(450);
+                $('#doc-tab-4').slideUp(450);
+                $('#doc-tab-5').slideUp(450);
+            });
+
+            $tab_2.on('click', function() {
+                $('#tab-1 .icon-box').removeClass('tab-active');
+                $('#tab-2 .icon-box').addClass('tab-active');
+                $('#tab-3 .icon-box').removeClass('tab-active');
+                $('#tab-4 .icon-box').removeClass('tab-active');
+                $('#tab-5 .icon-box').removeClass('tab-active');
+
+                $('#doc-tab-1').slideUp(450);
+                $('#doc-tab-2').slideToggle(450);
+                $('#doc-tab-3').slideUp(450);
+                $('#doc-tab-4').slideUp(450);
+                $('#doc-tab-5').slideUp(450);
+            });
+
+            $tab_3.on('click', function() {
+                $('#tab-1 .icon-box').removeClass('tab-active');
+                $('#tab-2 .icon-box').removeClass('tab-active');
+                $('#tab-3 .icon-box').addClass('tab-active');
+                $('#tab-4 .icon-box').removeClass('tab-active');
+                $('#tab-5 .icon-box').removeClass('tab-active');
+
+                $('#doc-tab-1').slideUp(450);
+                $('#doc-tab-2').slideUp(450);
+                $('#doc-tab-3').slideToggle(450);
+                $('#doc-tab-4').slideUp(450);
+                $('#doc-tab-5').slideUp(450);
+            });
+
+            $tab_4.on('click', function() {
+                $('#tab-1 .icon-box').removeClass('tab-active');
+                $('#tab-2 .icon-box').removeClass('tab-active');
+                $('#tab-3 .icon-box').removeClass('tab-active');
+                $('#tab-4 .icon-box').addClass('tab-active');
+                $('#tab-5 .icon-box').removeClass('tab-active');
+
+                $('#doc-tab-1').slideUp(450);
+                $('#doc-tab-2').slideUp(450);
+                $('#doc-tab-3').slideUp(450);
+                $('#doc-tab-4').slideToggle(450);
+                $('#doc-tab-5').slideUp(450);
+            });
+
+            $tab_5.on('click', function() {
+                $('#tab-1 .icon-box').removeClass('tab-active');
+                $('#tab-2 .icon-box').removeClass('tab-active');
+                $('#tab-3 .icon-box').removeClass('tab-active');
+                $('#tab-4 .icon-box').removeClass('tab-active');
+                $('#tab-5 .icon-box').addClass('tab-active');
+
+                $('#doc-tab-1').slideUp(450);
+                $('#doc-tab-2').slideUp(450);
+                $('#doc-tab-3').slideUp(450);
+                $('#doc-tab-4').slideUp(450);
+                $('#doc-tab-5').slideToggle(450);
+            });
         }
 
     }); // end document.ready
